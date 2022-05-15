584c8ad1cd8e67805b93c5d2fd04ed6cc754c755
Minghao Liu <mhliu93@gmail.com>
2022-05-15 12:36:11 +0800

    Pre-trained the model

--- simple_ml_model/main.py
+++ simple_ml_model/main.py
@@ -1,29 +1,18 @@
 from datetime import datetime
 import joblib
-import os 
 
 from utils.load_data import load_data
-from utils.preprocess_data import preprocess_data
-from utils.experiment import experiment
-from utils.fit_best_model import fit_best_model
 
-def train_model(): 
+def predict(): 
     # load data 
     df = load_data()
-    # train test split 
-    x_train, x_test, y_train, y_test = preprocess_data(df)
+    X = df.iloc[:,:-1]
 
-    # run experiments 
-    best_params = experiment(x_train, x_test, y_train, y_test)
-
-    # fit the best model 
-    best_model = fit_best_model(df, best_params)
-    # save best model
-    curr_dir = os.getcwd()
-    print(curr_dir)
-    now = datetime.now().strftime('%d-%m-%Y_%H:%M:%S')
-    filename = curr_dir + '/models/model_' + now + '.pkl'
-    joblib.dump(best_model, filename, compress=1)
+    # load best model
+    filename = 'models/best_model.pkl'
+    best_model = model = joblib.load(filename)
+    y_hat = best_model.predict(X)
+    print('prediction successful!')
 
 if __name__ == '__main__': 
-    train_model()
+    predict()
---
+++ simple_ml_model/train.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import joblib
+
+from utils.load_data import load_data
+from utils.preprocess_data import preprocess_data
+from utils.experiment import experiment
+from utils.fit_best_model import fit_best_model
+
+def train_model(): 
+    # load data 
+    df = load_data()
+    # train test split 
+    x_train, x_test, y_train, y_test = preprocess_data(df)
+
+    # run experiments 
+    best_params = experiment(x_train, x_test, y_train, y_test)
+
+    # fit the best model 
+    best_model = fit_best_model(df, best_params)
+    # save best model
+    now = datetime.now().strftime('%d-%m-%Y_%H:%M:%S')
+    filename = 'models/best_model.pkl'
+    joblib.dump(best_model, filename, compress=1)
+
+if __name__ == '__main__': 
+    train_model()
