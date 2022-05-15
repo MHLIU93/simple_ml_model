870bfde2e550f9c26e898fd6e69316056ee302f5
Minghao Liu <mhliu93@gmail.com>
2022-05-15 12:39:33 +0800

    Update main.py

--- simple_ml_model/main.py
+++ simple_ml_model/main.py
@@ -1,16 +1,18 @@
-from datetime import datetime
 import joblib
+import os
 
 from utils.load_data import load_data
+
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'models/best_model.pkl')
 
 def predict(): 
     # load data 
     df = load_data()
-    X = df.iloc[:,:-1]
+    X = df.iloc[:10,:-1]
 
     # load best model
-    filename = 'models/best_model.pkl'
-    best_model = model = joblib.load(filename)
+    best_model = joblib.load(filename)
     y_hat = best_model.predict(X)
     print('prediction successful!')
 
